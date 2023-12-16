4bc3cef6122c16a148d374362d5b3e81c2bcbeb5
SangHyunLee <22000516@handong.ac.kr>
2023-12-16 14:25:53 +0900

    저장 완성

---
+++ src/main/java/com/user/UserDAO.java
@@ -0,0 +1,20 @@
+package com.user;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+
+    private final SqlSessionTemplate sqlSession;
+
+    @Autowired
+    public UserDAO(SqlSessionTemplate sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public UserVO getUser(UserVO vo) {
+        return sqlSession.selectOne("getUser", vo);
+    }
+}
